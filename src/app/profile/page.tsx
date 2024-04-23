c56117678a44f8c181b7592fe32d6885b94e6482
pawantechofficial <pawan81222@gmail.com>
2024-04-23 13:53:26 +0530

    add profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="text-white pt-16 h-screen justify-center items-center bg-zinc-900 body-font">
+      <h1>This is Profile Page</h1>
+    </div>
+  );
+};
+
+export default page;
